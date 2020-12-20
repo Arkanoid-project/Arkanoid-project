a5e7eaa04d14da807d37025695e1bee3943a3aad
M0rEe <62582432+M0rEe@users.noreply.github.com>
2020-12-20 18:16:16 +0200

    added all buttons

--- Game/src/Menu.java
+++ Game/src/Menu.java
@@ -14,12 +14,14 @@ public class Menu extends JFrame implements MouseListener {
     JButton Start_btn=new JButton();
     JButton Settings_btn=new JButton();
     JButton Exit_btn=new JButton();
+    JButton ScoreB_btn=new JButton();
 
-    ImageIcon Start_img =new ImageIcon("C:\\Users\\Om562\\IdeaProjects\\Arkanoid-finale\\Game\\Data\\Start_button.png");
-    ImageIcon Setting_img =new ImageIcon("C:\\Users\\Om562\\IdeaProjects\\Arkanoid-finale\\Game\\Data\\s_button.png");
-    ImageIcon BackGround=new ImageIcon("C:\\Users\\Om562\\IdeaProjects\\Arkanoid-finale\\Game\\Data\\layer_1__semi_final.png");
-    ImageIcon Exit_img=new ImageIcon("C:\\Users\\Om562\\IdeaProjects\\Arkanoid-finale\\Game\\Data\\Score_button2.png");
-    JLabel Background_label=new JLabel("",BackGround,JLabel.CENTER);
+    ImageIcon Start_img =new ImageIcon("Game\\Data\\Start_button.png");
+    ImageIcon Setting_img =new ImageIcon("Game\\Data\\s_button.png");
+    ImageIcon BackGround=new ImageIcon("Game\\Data\\layer_1__semi_final.png");
+    ImageIcon Score_img=new ImageIcon("Game\\Data\\Score_button2.png");
+    ImageIcon Exit_img=new ImageIcon("Game\\Data\\exit_Button.png");
+    JLabel Background_label=new JLabel(BackGround);
 
         public Menu(){
 
@@ -34,40 +36,54 @@ public class Menu extends JFrame implements MouseListener {
 
         Background_label.setBounds(-10,-20,Frame_WIDTH,Frame_HEIGHT);
 
+
         Start_btn.setBounds(366,451,205,110);
 
         Start_btn.setBorderPainted(false);
-        Start_btn.setFocusPainted(false);
         Start_btn.addMouseListener(this);
         Start_btn.setIcon(Start_img);
         Start_btn.setBackground(Color.black);
         Start_btn.setFocusPainted(false);
         Start_btn.setOpaque(false);
+        Start_btn.setContentAreaFilled(false);
 
 
 
         Settings_btn.setBounds(666,113,100,80);
 
         Settings_btn.setBorderPainted(false);
-        Settings_btn.setFocusPainted(false);
         Settings_btn.addMouseListener(this);
         Settings_btn.setIcon(Setting_img);
         Settings_btn.setBackground(Color.black);
         Settings_btn.setFocusPainted(false);
         Settings_btn.setOpaque(false);
+        Settings_btn.setContentAreaFilled(false);
 
-        Exit_btn.setBounds(666,220,100,82);
+
+
+        ScoreB_btn.setBounds(666,240,100,82);
+
+        ScoreB_btn.setBorderPainted(false);
+        ScoreB_btn.addMouseListener(this);
+        ScoreB_btn.setIcon(Score_img);
+        ScoreB_btn.setBackground(Color.black);
+        ScoreB_btn.setFocusPainted(false);
+        ScoreB_btn.setOpaque(false);
+        ScoreB_btn.setContentAreaFilled(false);
+
+        Exit_btn.setBounds(666,370,100,82);
 
         Exit_btn.setBorderPainted(false);
-        Exit_btn.setFocusPainted(false);
         Exit_btn.addMouseListener(this);
         Exit_btn.setIcon(Exit_img);
         Exit_btn.setBackground(Color.black);
         Exit_btn.setFocusPainted(false);
         Exit_btn.setOpaque(false);
+        Exit_btn.setContentAreaFilled(false);
 
         this.add(Start_btn);
         this.add(Settings_btn);
+        this.add(ScoreB_btn);
         this.add(Exit_btn);
         this.add(Background_label);
         this.addMouseListener(this);
@@ -77,10 +93,14 @@ public class Menu extends JFrame implements MouseListener {
     public void mouseClicked(MouseEvent e) {
         if(e.getSource()==Start_btn){
             System.out.println("Start level");
+            //this.;
         }else if (e.getSource()==Settings_btn){
             System.out.println("Settings");
-        }else{
+        }else if(e.getSource()==ScoreB_btn){
+            System.out.println("Score");
+        }else {
             System.out.println("Exit");
+            this.dispose();
         }
     }
 
@@ -100,12 +120,15 @@ public class Menu extends JFrame implements MouseListener {
             System.out.println("BigSettings");
         }else if (e.getSource()==Exit_btn){
             Exit_btn.setBounds(Exit_btn.getX(),Exit_btn.getY(),Exit_btn.getWidth()+10,Exit_btn.getHeight()+10);
-
             System.out.println("BigExit");
-        }else{
+        }else if(e.getSource()==ScoreB_btn) {
+            ScoreB_btn.setBounds(ScoreB_btn.getX(),ScoreB_btn.getY(),ScoreB_btn.getWidth()+10,ScoreB_btn.getHeight()+10);
+            System.out.println("BigScore");
+        } else{
             Start_btn.setBounds(366,451,205,110);
             Settings_btn.setBounds(666,113,100,80);
-            Exit_btn.setBounds(666,220,100,82);
+            Exit_btn.setBounds(666,370,100,82);
+            ScoreB_btn.setBounds(666,240,100,82);
 
         }
     }
