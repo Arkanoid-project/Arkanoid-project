71e91f2d2106a958f49a006d9f56d78f59132010
M0rEe <62582432+M0rEe@users.noreply.github.com>
2020-12-20 20:11:04 +0200

    added panels for all buttons to be drawn

--- Game/src/Menu.java
+++ Game/src/Menu.java
@@ -6,10 +6,12 @@ import java.awt.event.MouseListener;
 public class Menu extends JFrame implements MouseListener {
     public final int Frame_HEIGHT=600;
     public final int Frame_WIDTH=800;
-
+    public int Scene_id=0;
 
     public final Dimension My_Dimension=new Dimension(Frame_WIDTH,Frame_HEIGHT);
-
+    JPanel main_panel=new JPanel();
+    JPanel settings_panel=new JPanel();
+    JPanel score_panel=new JPanel();
 
     JButton Start_btn=new JButton();
     JButton Settings_btn=new JButton();
@@ -21,84 +23,104 @@ public class Menu extends JFrame implements MouseListener {
     ImageIcon BackGround=new ImageIcon("Game\\Data\\layer_1__semi_final.png");
     ImageIcon Score_img=new ImageIcon("Game\\Data\\Score_button2.png");
     ImageIcon Exit_img=new ImageIcon("Game\\Data\\exit_Button.png");
-    JLabel Background_label=new JLabel(BackGround);
+    JLabel Background_label=new JLabel();
 
         public Menu(){
-
+        main_panel.setBounds(0,0,Frame_WIDTH,Frame_HEIGHT);
         this.setTitle("Arkanoid");
         this.setSize(My_Dimension);
         this.setResizable(true);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().setBackground(Color.BLACK);
-        this.setLayout(null);
-
-        Background_label.setBounds(-10,-20,Frame_WIDTH,Frame_HEIGHT);
-
-
-        Start_btn.setBounds(366,451,205,110);
-
-        Start_btn.setBorderPainted(false);
-        Start_btn.addMouseListener(this);
-        Start_btn.setIcon(Start_img);
-        Start_btn.setBackground(Color.black);
-        Start_btn.setFocusPainted(false);
-        Start_btn.setOpaque(false);
-        Start_btn.setContentAreaFilled(false);
-
-
-
-        Settings_btn.setBounds(666,113,100,80);
-
-        Settings_btn.setBorderPainted(false);
-        Settings_btn.addMouseListener(this);
-        Settings_btn.setIcon(Setting_img);
-        Settings_btn.setBackground(Color.black);
-        Settings_btn.setFocusPainted(false);
-        Settings_btn.setOpaque(false);
-        Settings_btn.setContentAreaFilled(false);
-
-
-
-        ScoreB_btn.setBounds(666,240,100,82);
-
-        ScoreB_btn.setBorderPainted(false);
-        ScoreB_btn.addMouseListener(this);
-        ScoreB_btn.setIcon(Score_img);
-        ScoreB_btn.setBackground(Color.black);
-        ScoreB_btn.setFocusPainted(false);
-        ScoreB_btn.setOpaque(false);
-        ScoreB_btn.setContentAreaFilled(false);
-
-        Exit_btn.setBounds(666,370,100,82);
-
-        Exit_btn.setBorderPainted(false);
-        Exit_btn.addMouseListener(this);
-        Exit_btn.setIcon(Exit_img);
-        Exit_btn.setBackground(Color.black);
-        Exit_btn.setFocusPainted(false);
-        Exit_btn.setOpaque(false);
-        Exit_btn.setContentAreaFilled(false);
-
-        this.add(Start_btn);
-        this.add(Settings_btn);
-        this.add(ScoreB_btn);
-        this.add(Exit_btn);
-        this.add(Background_label);
         this.addMouseListener(this);
+        this.add(main_panel);
+        this.add(settings_panel);
+        this.add(score_panel);
+        settings_panel.setVisible(false);
+        score_panel.setVisible(false);
+        this.drawScene(Scene_id);
+    }
+    public void drawScene(int Scene_id){
+        switch (Scene_id) {
+            case 0 -> {
+                Background_label.setIcon(BackGround);
+                Background_label.setPreferredSize(My_Dimension);
+                Start_btn.setBounds(366, 451, 205, 110);
+                Start_btn.setBorderPainted(false);
+                Start_btn.addMouseListener(this);
+                Start_btn.setIcon(Start_img);
+                Start_btn.setBackground(Color.black);
+                Start_btn.setFocusPainted(false);
+                Start_btn.setOpaque(false);
+                Start_btn.setContentAreaFilled(false);
+                Settings_btn.setBounds(666, 113, 100, 80);
+                Settings_btn.setBorderPainted(false);
+                Settings_btn.addMouseListener(this);
+                Settings_btn.setIcon(Setting_img);
+                Settings_btn.setBackground(Color.black);
+                Settings_btn.setFocusPainted(false);
+                Settings_btn.setOpaque(false);
+                Settings_btn.setContentAreaFilled(false);
+                ScoreB_btn.setBounds(666, 240, 100, 82);
+                ScoreB_btn.setBorderPainted(false);
+                ScoreB_btn.addMouseListener(this);
+                ScoreB_btn.setIcon(Score_img);
+                ScoreB_btn.setBackground(Color.black);
+                ScoreB_btn.setFocusPainted(false);
+                ScoreB_btn.setOpaque(false);
+                ScoreB_btn.setContentAreaFilled(false);
+                Exit_btn.setBounds(666, 370, 100, 82);
+                Exit_btn.setBorderPainted(false);
+                Exit_btn.addMouseListener(this);
+                Exit_btn.setIcon(Exit_img);
+                Exit_btn.setBackground(Color.black);
+                Exit_btn.setFocusPainted(false);
+                Exit_btn.setOpaque(false);
+                Exit_btn.setContentAreaFilled(false);
+                Background_label.add(Start_btn);
+                Background_label.add(Settings_btn);
+                Background_label.add(ScoreB_btn);
+                Background_label.add(Exit_btn);
+                main_panel.add(Background_label);
+            }
+            case 1 -> {
+                //choose level panel
+            }
+            case 2 ->{
+                //settings panel
+                settings_panel.setBounds(0,0,Frame_WIDTH,Frame_HEIGHT);
+                settings_panel.setBackground(Color.GREEN);
+            }
+            case 3->{
+                //scoreboard panel
+                score_panel.setBounds(0,0,Frame_WIDTH,Frame_HEIGHT);
+                score_panel.setBackground(Color.BLUE);
+            }
+        }
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         if(e.getSource()==Start_btn){
             System.out.println("Start level");
-            //this.;
+            main_panel.setVisible(false);
+            Scene_id=1;
+            System.out.println(Scene_id);
+
         }else if (e.getSource()==Settings_btn){
+            main_panel.setVisible(false);
+            settings_panel.setVisible(true);
+            Scene_id=2;
+            drawScene(Scene_id);
             System.out.println("Settings");
         }else if(e.getSource()==ScoreB_btn){
+            main_panel.setVisible(false);
+            score_panel.setVisible(true);
+            Scene_id=3;
+            drawScene(Scene_id);
             System.out.println("Score");
-        }else {
+        }else if(e.getSource()==Exit_btn) {
             System.out.println("Exit");
             this.dispose();
         }
